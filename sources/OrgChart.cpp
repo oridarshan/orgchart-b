36f3328d1ea4804c7dea88e6a96c5fc2efb916ba
Ori Darshan <oridarshan@gmail.com>
2022-06-07 17:17:49 +0300

    implement iterators

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -30,12 +30,15 @@ namespace ariel
     OrgChart& OrgChart::add_root(std::string name)
     {
         if (_data.size() == 0){
-            Node *root =new Node(name);
-            root->newline = true;
-            _data.push_back(root);
+            Node *new_root =new Node(name);
+            new_root->level = 0;
+            _data.push_back(new_root);
+            root = new_root;
+            last_level = new_root;
         }
         else{
             _data.at(0)->name = name;
+            root->name = name;
         }
         return *this;
     }
@@ -54,6 +57,7 @@ namespace ariel
         // construct new node
         Node *new_node =new Node(name);
         new_node->father = predecessor;
+        new_node->level = predecessor->level+1;
         predecessor->children.push_back(new_node);
         // add node and update prev-next to all nodes
         // _data.push_back(new_node);
@@ -79,6 +83,16 @@ namespace ariel
                 new_order.at(i)->prev = new_order.at(i-1);
             }
         }
+        root = new_order.front();
+        for (size_t i = new_order.size() - 1; i >= 1; i--)
+        {
+            if (new_order.at(i)->level != new_order.at(i-1)->level)
+            {
+                last_level = new_order.at(i);
+                break;
+            }
+            if(i == 0){throw "only 1 level after adding sub";}
+        }
         
         _data = new_order;
         //TODO delete prints
@@ -86,6 +100,51 @@ namespace ariel
                 
         return *this;
     }
+
+    OrgChart::reverse_level_order_iterator& OrgChart::reverse_level_order_iterator::operator++(){
+        if (current_node_p->next &&
+             current_node_p->next->level == current_node_p->level)
+        {
+            current_node_p = current_node_p->next;
+            return *this;
+        }
+        if (current_node_p->father)
+        {
+            Node *next_rev = current_node_p->father;
+            while (next_rev->prev &&
+                 next_rev->prev->level== next_rev->level)
+            {
+                next_rev = next_rev->prev;
+            }
+            current_node_p = next_rev;
+            return *this;
+        }
+        // we are at root
+        current_node_p = nullptr;
+        return *this;
+        }
+    
+    OrgChart::preorder_iterator& OrgChart::preorder_iterator::operator++()
+    {
+        Node *next = next_pre_order(this->current_node_p);
+        current_node_p = next;
+        return *this;
+    } 
+
+    OrgChart::Node* OrgChart::preorder_iterator::next_pre_order(Node * curr_node){
+        if(!curr_node){return nullptr;}
+        //loop over chilren, return first unmarked (and mark it)
+        for (Node *pchild : curr_node->children){
+            if (!pchild->mark)
+            {
+                pchild->mark = true;
+                return pchild;
+            }
+        }
+        //already visited all sub-trees, find next child for father
+        return next_pre_order(curr_node->father);
+    }
+
 
 
     std::ostream& operator<< (std::ostream& output, const OrgChart& org)
--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -16,12 +16,17 @@ namespace ariel
             Node* next = nullptr;
             Node* prev = nullptr;
             std::vector<Node*> children;
-            bool newline = false;
+            int level;
             bool mark = false;
             Node(std::string name):name(name){}
             };
         // vector stores data by level order
         std::vector<Node*> _data;
+        // root and beginning of last level
+        Node *root;
+        Node *last_level;
+        // Return next step in pre-order
+        Node* next_pre_order(Node * curr_node){return curr_node;}
         // TODO search for a node, return reference
         Node* search_node(std::string name);
         // Node* search_node(std::string name)
@@ -39,7 +44,7 @@ namespace ariel
         //     return pnode;
         // }
     public:
-        OrgChart():_mod_count(0){}
+        OrgChart():_mod_count(0),root(nullptr),last_level(nullptr){}
         ~OrgChart();
         OrgChart& add_root(std::string name);
         OrgChart& add_sub(std::string name, std::string father);
@@ -61,9 +66,11 @@ namespace ariel
             // binary operators
             bool operator==(const level_order_iterator& itr) const{
                 return current_node_p == itr.current_node_p;
+                // return true;
             }
             bool operator!=(const level_order_iterator& itr) const{
                 return !(*this == itr);
+                // return true;
             };
         
         };
@@ -79,9 +86,7 @@ namespace ariel
             // unary operators
             const std::string& operator*() const {return current_node_p->name;} 
             const std::string* operator->() const {return &(current_node_p->name);} 
-            reverse_level_order_iterator& operator++(){
-                current_node_p = current_node_p->prev;
-                return *this;} 
+            reverse_level_order_iterator& operator++();
             // binary operators
             bool operator==(const reverse_level_order_iterator& itr) const{
                 return current_node_p == itr.current_node_p;
@@ -97,12 +102,14 @@ namespace ariel
         {
         private:
             Node* current_node_p;
+            // Return next step in pre-order
+            Node* next_pre_order(Node * curr_node);
         public:
             preorder_iterator(Node* ptr = nullptr):current_node_p(ptr){}            
             // unary operators
             const std::string& operator*() const {return current_node_p->name;} 
             const std::string* operator->() const {return &(current_node_p->name);} 
-            preorder_iterator& operator++(){return *this;} 
+            preorder_iterator& operator++();
             // binary operators
             bool operator==(const preorder_iterator& itr) const{
                 return false;
@@ -114,15 +121,15 @@ namespace ariel
         };
 
 
-        level_order_iterator begin(){return level_order_iterator((_data.at(0)));}
+        level_order_iterator begin(){return level_order_iterator((root));}
         level_order_iterator end(){return level_order_iterator(nullptr);}
         level_order_iterator begin_level_order(){return begin();}
         level_order_iterator end_level_order(){return end();}
-        reverse_level_order_iterator begin_reverse_order(){return reverse_level_order_iterator(_data.back());}
-        reverse_level_order_iterator end_reverse_order(){return reverse_level_order_iterator();}
+        reverse_level_order_iterator begin_reverse_order(){return reverse_level_order_iterator(last_level);}
+        reverse_level_order_iterator end_reverse_order(){return nullptr;}
         //cover for mistake in Demo.cpp
         reverse_level_order_iterator reverse_order(){return end_reverse_order();}
-        preorder_iterator begin_preorder(){return preorder_iterator((_data.at(0)));}
+        preorder_iterator begin_preorder(){return preorder_iterator((root));}
         preorder_iterator end_preorder(){return preorder_iterator(nullptr);}
         
         friend std::ostream& operator<< (std::ostream& output, const OrgChart& org);    
